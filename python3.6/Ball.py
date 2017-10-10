bc30e5a73fd6748f06e600c397ad308b0b670b56
Roncarlos <techer.jeancharles974@gmail.com>
2017-10-10 16:07:59 +0400

    Update comments

--- python3.6/Ball.py
+++ python3.6/Ball.py
@@ -1,7 +1,10 @@
 import math
 
 class Ball:
-
+    """
+    Ball class
+    Representation of a Ball
+    """
     def __init__(self, x, y, r):
         self.x          = x
         self.y          = y
@@ -13,28 +16,53 @@ class Ball:
         self.color      = "#000000"
 
     def updateDeplacement(self):
+        """
+        Calcul of d_x and d_y for moving
+        """
         self.d_x = self.speed * math.cos(self.direction)
         self.d_y = self.speed * math.sin(self.direction)
 
     def move(self):
+        """
+        Move the ball
+        """
         self.x += self.d_x
         self.y += self.d_y
 
     def invertDx(self):
+        """
+        change the direction of the ball for x coord
+        """
         self.d_x = -self.d_x
 
     def invertDy(self):
+        """
+        change the direction of the ball for y coord
+        """
         self.d_y = -self.d_y
 
     def setColor(self, color):
+        """
+        set the color of the ball by the argument given
+        """
         self.color = color
 
     def setSpeed(self, speed):
+        """
+        set the speed of the ball by the argument given
+        """
         self.speed = speed
 
     def setDirection(self, d):
+        """
+        set the direction (in degree) of the ball by the argument given
+        and update dx and dy
+        """
         self.direction = d
         self.updateDeplacement()
 
     def setGraphics(self, graphics):
+        """
+        set the graphic (ellipse object) of the ball by the argument given
+        """
         self.graphics = graphics
